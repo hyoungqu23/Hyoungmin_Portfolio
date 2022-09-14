877e964c18a772debd0e30b15dc703a3e9b07bb6
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-09-14 17:33:35 +0900

    Env: tailwind config에서 기본 색상 설정
    - 글자 색: #CFDEF3
    - 배경 색: #414345

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#CFDEF3',
+          1: '#414345',
+        },
+      },
+    },
   },
   plugins: [],
 };
