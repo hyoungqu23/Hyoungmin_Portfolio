e44ca90e8c5e3194e71cd297b73db71731837eed
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-09-15 23:52:52 +0900

    Feat: 라우트 상수화 처리 및 적용
    - 라우트를 상수화하여 유지보수와 통일성을 부여

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -1,24 +1,25 @@
 import Link from 'next/link';
+import { ROUTES } from '../../../constants/routes';
 
 const Header = () => {
   return (
     <header className="flex justify-between font-bold text-base md:text-xl lg:text-2xl px-2 md:px-5">
-      <Link href="/">
+      <Link href={ROUTES.HOME}>
         <a>이형민</a>
       </Link>
       <nav>
         <ul className="flex gap-5 md:gap-16 lg:gap-20">
           <li>
-            <Link href="/about">소개</Link>
+            <Link href={ROUTES.ABOUT}>소개</Link>
           </li>
           <li>
-            <Link href="/projects">프로젝트</Link>
+            <Link href={ROUTES.PROJECTS}>프로젝트</Link>
           </li>
           <li>
-            <Link href="/experience">경험</Link>
+            <Link href={ROUTES.EXPERIENCE}>경험</Link>
           </li>
           <li>
-            <Link href="/more">더 알아보기</Link>
+            <Link href={ROUTES.MORE}>더 알아보기</Link>
           </li>
         </ul>
       </nav>
