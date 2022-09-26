fe383c4fa0d0c4bf08bc40eb87942c1a9c4f1670
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-09-26 23:12:46 +0900

    Feat: 메인 페이지 프로필 이미지 애니메이션 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,23 @@
 import type { NextPage } from 'next';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 const Home: NextPage = () => {
   return (
-    <div className="container h-full flex flex-col justify-center items-center">
-      <div className="overflow-hidden mb-5 lg:mb-8">
+    <div className="container flex flex-col items-center justify-center h-full">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1 }}
+        className="mb-5 overflow-hidden lg:mb-8"
+      >
         <Image src="/image/profile.svg" width={'250%'} height={'250%'} />
-      </div>
-      <h2 className="text-3xl lg:text-4xl mb-3 lg:mb-5">
-        <span className="font-bold hover:border-b-8 border-yellow-300">프론트엔드</span> 개발자
+      </motion.div>
+      <h2 className="mb-3 text-3xl lg:text-4xl lg:mb-5">
+        <span className="font-bold border-yellow-300 hover:border-b-8">프론트엔드</span> 개발자
       </h2>
       <h1 className="text-5xl lg:text-7xl">
-        <span className="font-bold hover:border-b-8 border-yellow-300">이형민</span>입니다
+        <span className="font-bold border-yellow-300 hover:border-b-8">이형민</span>입니다
       </h1>
     </div>
   );
