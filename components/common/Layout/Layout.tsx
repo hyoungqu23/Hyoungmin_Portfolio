e2c03546d5a1553df19e18b612ae78c945ecdaa7
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-03 20:42:41 +0900

    Fix: 페이지 전환 애니메이션이 모바일에서 깨지는 이슈로 인해 제거

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,33 +1,11 @@
 import Head from 'next/head';
 import Header from 'components/common/Header/Header';
 import Footer from 'components/common/Footer/Footer';
-import { motion } from 'framer-motion';
-import { useRouter } from 'next/router';
 import { PropsWithChildren } from 'react';
 
-const pageSwitchAnimation = {
-  initialState: {
-    opacity: 0,
-  },
-  animateState: {
-    opacity: 1,
-  },
-};
-
 const Layout = (props: PropsWithChildren) => {
-  const router = useRouter();
-
   return (
-    <motion.div
-      key={router.route}
-      initial="initialState"
-      animate="animateState"
-      transition={{
-        duration: 0.5,
-      }}
-      variants={pageSwitchAnimation}
-      className="h-screen"
-    >
+    <div className="h-screen">
       <Head>
         <title>PortFolio - HyoungMin</title>
         <meta name="description" content="Hyoungmin's PortFolio" />
@@ -36,7 +14,7 @@ const Layout = (props: PropsWithChildren) => {
       <Header />
       <main className="container h-[80vh] py-2 w-4/5 m-auto">{props.children}</main>
       <Footer />
-    </motion.div>
+    </div>
   );
 };
 
