11a5c292974faccec4ee75d2657a3b3c997b85ea
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-03 23:03:02 +0900

    Feat: Header의 로고 애니메이션 추가

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -1,24 +1,18 @@
 import Link from 'next/link';
-import { motion } from 'framer-motion';
+import { Typewriter } from 'react-simple-typewriter';
 import { ROUTES } from 'constants/routes';
 
 const Header = () => {
   return (
-    <header className="flex h-[7.5vh] justify-between items-center px-2 md:px-5">
-      <motion.div
-        initial={{ x: -500, opacity: 0, scale: 0.5 }}
-        animate={{ x: 0, opacity: 1, scale: 1 }}
-        transition={{ duration: 1 }}
-        className="text-base font-bold tracking-widest md:text-2xl lg:text-3xl"
-      >
-        <Link href={ROUTES.HOME}>이형민</Link>
-      </motion.div>
-      <motion.nav
-        initial={{ x: 500, opacity: 0, scale: 0.5 }}
-        animate={{ x: 0, opacity: 1, scale: 1 }}
-        transition={{ duration: 1 }}
-      >
-        <ul className="flex gap-5 text-base md:gap-16 lg:gap-20 md:text-lg lg:text-xl">
+    <header className="sticky top-0 flex h-[10vh] w-4/5 mx-auto justify-between items-center">
+      <div className="text-base font-bold tracking-widest md:text-2xl lg:text-3xl">
+        <Typewriter words={['hyoungmin']} />
+      </div>
+      <nav>
+        <ul className="flex gap-5 text-base lg:gap-10 lg:text-lg">
+          <li>
+            <a href={ROUTES.HOME}>홈</a>
+          </li>
           <li>
             <Link href={ROUTES.ABOUT}>소개</Link>
           </li>
@@ -32,7 +26,7 @@ const Header = () => {
             <Link href={ROUTES.MORE}>더 알아보기</Link>
           </li>
         </ul>
-      </motion.nav>
+      </nav>
     </header>
   );
 };
