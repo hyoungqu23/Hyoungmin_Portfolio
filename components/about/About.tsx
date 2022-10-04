d3d9473708af55bce621d3b787b62aba79171fe9
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-04 14:12:42 +0900

    Feat: About 컴포넌트의 내용 애니메이션 추가

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -10,12 +10,17 @@ const About = () => {
       id="about"
       className="container flex flex-row gap-10 items-center justify-center h-[90vh]"
     >
-      <div className="flex flex-col w-1/2 gap-3">
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1.2 }}
+        className="flex flex-col w-1/2 gap-3"
+      >
         <h2 className="text-xl lg:text-2xl">안녕하세요, 프론트엔드 개발자 이형민입니다.🙋‍♂️</h2>
         {aboutDetailData.map((data, index) => (
           <AboutDetail key={index} data={data} />
         ))}
-      </div>
+      </motion.div>
       <motion.div
         initial={{ x: 500, opacity: 0 }}
         whileInView={{ x: 0, opacity: 1 }}
