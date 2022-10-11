433427d50685979aca03ae2712b6b044b54d8731
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-11 12:00:39 +0900

    Fix: About Section UI 수정
    - data 변경에 따른 flex 수정

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -1,33 +1,36 @@
 import React from 'react';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
-import AboutDetail from 'components/about/AboutDetail/AboutDetail';
+import AboutDetail from 'components/about/AboutDetail';
 import { aboutDetailData } from 'data/aboutData';
 
 const About = () => {
   return (
     <section
       id="about"
-      className="container flex flex-col-reverse lg:flex-row gap-10 items-center justify-center h-[90vh]"
+      className="container relative flex flex-col-reverse lg:flex-row gap-10 items-center justify-center h-[90vh]"
     >
+      <h3 className="absolute text-3xl tracking-widest uppercase top-10">About</h3>
       <motion.div
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ duration: 1.2 }}
-        className="flex flex-col w-full gap-3 lg:w-1/2"
+        className="flex flex-col w-full lg:w-2/3"
       >
-        <h2 className="text-base lg:text-2xl">안녕하세요🙋‍♂️ 프론트엔드 개발자 이형민입니다.</h2>
-        {aboutDetailData.map((data, index) => (
-          <AboutDetail key={index} data={data} />
-        ))}
+        <h2 className="mb-5 text-base lg:text-3xl">안녕하세요🙋‍♂️ 프론트엔드 개발자 이형민입니다.</h2>
+        <div className="flex flex-col gap-2 lg">
+          {aboutDetailData.map((data, index) => (
+            <AboutDetail key={index} data={data} />
+          ))}
+        </div>
       </motion.div>
       <motion.div
-        initial={{ x: '50%', opacity: 0 }}
+        initial={{ x: '500', opacity: 0 }}
         whileInView={{ x: 0, opacity: 1 }}
         transition={{ duration: 1.2 }}
-        className="w-1/2 overflow-hidden"
+        className="overflow-hidden"
       >
-        <Image src="/image/profile.svg" width={'1000%'} height={'1000%'} />
+        <Image src="/image/profile.svg" width={'500%'} height={'500%'} />
       </motion.div>
     </section>
   );
