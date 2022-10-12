5ce44fcb1c2b754c23ba108dbb441a15994cd602
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-12 20:38:46 +0900

    Chore: 오타 수정

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="flex w-4/5 mx-auto h-[50px] items-center flex-row justify-between p-1 gap-0 md:gap-2">
       <div className="w-full text-xs text-center">
-        Copyright 2022. Hyougmin. All rights reserved.
+        Copyright 2022. Hyoungmin. All rights reserved.
       </div>
     </footer>
   );
