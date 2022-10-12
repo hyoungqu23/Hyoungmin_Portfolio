f4c788731d9d734edd23830df78714606afdbc92
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-12 16:07:45 +0900

    Fix: 섹션 타이틀 수정
    - 모바일 뷰에서 겹치는  이슈로 인해 top 높이 조정

--- components/experience/Experience.tsx
+++ components/experience/Experience.tsx
@@ -7,7 +7,7 @@ const Experience = () => (
     id="experience"
     className="container relative flex flex-col items-center justify-center h-screen gap-20"
   >
-    <h3 className="absolute text-2xl tracking-widest uppercase top-10">Experience</h3>
+    <h3 className="absolute text-2xl tracking-widest uppercase top-5 md:top-10">Experience</h3>
     {educationData.map((data, index) => (
       <ExperienceBox key={data.program} education={data} index={index} />
     ))}
