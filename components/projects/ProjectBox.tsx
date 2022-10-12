b67f67403462a2ced0fb32bd261a0f9663e936d7
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-12 18:15:43 +0900

    Fix: 프로젝트 섹션 리뉴얼
    - 슬라이더 삭제
    - 스크롤 애니메이션 추가
    - 이미지 추가
    - 회고, 설명 추가
    - 이력서와 유사한 내용만 있어서 별도로 데이터를 수정하고, 이미지를 추가하는 등 피드백 반영

---
+++ components/projects/ProjectBox.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import type { Projects } from 'data/projectData';
+
+type Props = {
+  project: Projects;
+};
+
+const ProjectBox = ({ project }: Props) => {
+  const { title, descriptions, retrospective, stack, period, type, imgUrl, url } = project;
+
+  const { githubUrl, retroUrl } = url;
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen gap-2 mt-10 md:even:flex-row md:odd:flex-row-reverse">
+      <motion.div
+        initial={{ y: -50, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1.2 }}
+        className="relative w-40 h-40 overflow-hidden md:w-2/3 md:h-2/3"
+      >
+        <a href={githubUrl}>
+          <Image
+            src={
+              imgUrl
+                ? imgUrl
+                : 'https://media.graphassets.com/output=format:jpg/Z8r7JPjZQgiDV4BXtQGF'
+            }
+            layout="fill"
+          />
+        </a>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        transition={{ duration: 1.2 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
+        className="px-0 text-center md:w-1/3 md:px-10"
+      >
+        <h4 className="text-xl font-semibold text-center">
+          <a href={githubUrl}>{title}</a>
+        </h4>
+        <p className="text-xs text-gray-400">{type}</p>
+        <p className="text-xs text-gray-400">{period}</p>
+        {stack.map(skill => (
+          <span key={skill} className="mr-1 text-xs text-sky-500">
+            {skill}
+          </span>
+        ))}
+        <div className="text-sm text-left text-gray-600 dark:text-gray-400 text-bold">
+          {retrospective}
+        </div>
+        <div className="text-left">
+          {descriptions.map((description, index) => (
+            <p
+              key={description.tag + index}
+              className="my-2 text-xs text-gray-400 dark:text-gray-500 md:text-sm"
+            >
+              {description.content}
+            </p>
+          ))}
+        </div>
+        <a
+          className="p-1 text-sm rounded bg-sky-500 text-sky-100 hover:text-sky-500 hover:bg-sky-100"
+          href={retroUrl}
+          target="_blank"
+        >
+          {title} 회고 보러가기
+        </a>
+      </motion.div>
+    </div>
+  );
+};
+
+export default ProjectBox;
--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import ProjectSlide from 'components/projects/ProjectSlide';
+import ProjectBox from 'components/projects/ProjectBox';
 import projects from 'data/projectData';
 
 const Projects = () => {
   return (
     <section
       id="projects"
-      className="container relative flex flex-col items-center h-screen max-w-full gap-20 mx-auto overflow-hidden justify-evenly md:flex-row"
+      className="container relative flex flex-col items-center h-full max-w-full gap-20 mx-auto overflow-hidden justify-evenly md:flex-row"
     >
-      <h3 className="absolute text-2xl tracking-widest uppercase top-24">Projects</h3>
-      <div className="relative flex w-full mt-10 overflow-scroll overflow-y-hidden snap-x snap-mandatory">
+      <h3 className="absolute text-2xl tracking-widest uppercase top-10 md:top-14">Projects</h3>
+      <p className="absolute text-xs text-gray-500 top-20 md:top-24">
+        프로젝트 이미지를 클릭하면 <span className="text-sky-500">프로젝트 레포지토리</span>로
+        연결됩니다.
+      </p>
+      <div className="flex flex-col gap-0">
         {projects.map(project => (
-          <ProjectSlide key={project.title} project={project} />
+          <ProjectBox key={project.title} project={project} />
         ))}
       </div>
     </section>
