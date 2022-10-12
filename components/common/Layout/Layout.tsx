df36991ab816b39fdfba3c9c96c573879395b74b
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-12 20:38:34 +0900

    Feat: 메타 태그 설정

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -2,13 +2,23 @@ import Head from 'next/head';
 import Header from 'components/common/Header/Header';
 import Footer from 'components/common/Footer/Footer';
 import { PropsWithChildren } from 'react';
+import profile from 'public/icons/profile.svg';
 
 const Layout = (props: PropsWithChildren) => {
   return (
     <>
       <Head>
-        <title>HyoungMin</title>
-        <meta name="description" content="Hyoungmin's PortFolio" />
+        <title>HyoungMin's Portfolio</title>
+        <meta
+          name="description"
+          content="안녕하세요, 피드백과 협업을 즐기는 프론트엔드 개발자 이형민입니다."
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content="Hyoungmin's Portfolio" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://hyoungmin-portfolio.vercel.app/" />
+        <meta property="og:image" content={profile} />
+        <meta property="og:article:author" content="Hyoungmin" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
       <Header />
