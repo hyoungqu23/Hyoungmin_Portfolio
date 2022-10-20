634deb5ee9b6eddcd9b506b502eb75bc245acbd7
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-20 16:30:57 +0900

    Fix: 링크 수정

--- data/educationData.ts
+++ data/educationData.ts
@@ -2,7 +2,7 @@ export interface EducationData {
   institute: string;
   period: string;
   program: string;
-  notionUrl?: string;
+  retroUrl?: string;
 }
 
 export const educationData: EducationData[] = [
@@ -10,12 +10,13 @@ export const educationData: EducationData[] = [
     institute: '원티드',
     program: '프리온보딩 프론트엔드 챌린지',
     period: '2022.10.04 - 2022.10.15',
+    retroUrl: 'https://minilog.vercel.app/blog/preonboarding-challenge-retrospective',
   },
   {
     institute: '원티드',
     program: '프리온보딩 프론트엔드 코스',
     period: '2022.08.29 - 2022.09.30',
-    notionUrl: 'https://hyoungmin.notion.site/9a60af68222c403292e2f638a274b462',
+    retroUrl: 'https://minilog.vercel.app/blog/preonboarding-retrospective',
   },
   {
     institute: '엘리스',
