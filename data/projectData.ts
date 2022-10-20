fd5f904b291a92eff98dcdf916c467dc3f7a8afb
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-20 16:30:05 +0900

    Public: 미니로그 프로젝트 데이터 추가

--- data/projectData.ts
+++ data/projectData.ts
@@ -1,4 +1,5 @@
 import { StaticImageData } from 'next/image';
+import Project_MiniLog from 'public/images/Project_MiniLog.png';
 import Project_Portfolio from 'public/images/Project_Portfolio.png';
 import Project_Router from 'public/images/Project_Router.png';
 import Project_Admin from 'public/images/Project_Admin.png';
@@ -33,6 +34,36 @@ export interface Projects {
 
 const projects: Projects[] = [
   {
+    title: '미니로그',
+    stack: ['Next.js', 'TypeScript', 'React', 'Tailwind CSS', 'Notion API'],
+    period: '2022.10.09 ~ 진행중',
+    type: '개인 프로젝트 / 프론트엔드',
+    imgUrl: Project_MiniLog,
+    url: {
+      pageUrl: 'https://minilog.vercel.app/',
+      githubUrl: 'https://github.com/hyoungqu23/miniLog',
+      retroUrl: 'https://minilog.vercel.app/blog/minilog-ssg-retrospective',
+    },
+    descriptions: [
+      {
+        tag: 'feature',
+        content:
+          '최적화된 웹 사이트를 만들고자 웹 접근성과 SEO를 최대한 향상시켜 Lighthouse에서 100점을 받았습니다.',
+      },
+      {
+        tag: 'feature',
+        content: 'First Load JavaScript를 약 4.5배 감소시켜 웹 사이트의 성능을 향상시켰습니다.',
+      },
+      {
+        tag: 'feature',
+        content:
+          'Notion API를 통해 Notion을 CMS로 활용하는 정적 블로그 형태를 선택해 보다 효율적으로 블로그 글 작성을 할 수 있도록 구현했습니다.',
+      },
+    ],
+    retrospective:
+      'SSG 구조와 Next.js의 여러 기능 등을 활용해 최적화된 웹 사이트를 만들고자 고민하고 노력했습니다. Notion API를 연결하면서 발생한 Axios, TypeScript와 관련된 여러 이슈들을 해결하고자 노력했습니다. 자세한 회고와 트러블슈팅은 아래 링크에서 보실 수 있습니다.',
+  },
+  {
     title: '프론트엔드 개발자 이형민입니다',
     stack: ['Next.js', 'TypeScript', 'React', 'Tailwind CSS'],
     period: '2022.09.29 - 2022.10.13',
@@ -41,20 +72,26 @@ const projects: Projects[] = [
     url: {
       pageUrl: 'https://hyoungmin.vercel.app/',
       githubUrl: 'https://github.com/hyoungqu23/Hyoungmin_Portfolio',
+      retroUrl: 'https://minilog.vercel.app/blog/portfolio-retrospective',
     },
     descriptions: [
       {
         tag: 'feature',
         content:
-          '개발자로서 자신의 프로젝트와 관련 정보를 제공하기 위해 포트폴리오 웹 사이트를 제작했습니다.',
+          '개발자로서 자신을 어필하기 위해 자신의 프로젝트와 관련 정보를 제공하고자 포트폴리오 웹 사이트를 제작했습니다.',
       },
       {
         tag: 'feature',
-        content: '모바일 뷰를 고려해 Tailwind CSS를 활용하여 반응형 디자인을 구현했습니다.',
+        content: '모바일 뷰를 고려해 Tailwind CSS를 활용하여 반응형 디자인을 쉽게 구현했습니다.',
+      },
+      {
+        tag: 'feature',
+        content:
+          '다양한 사람들에게 피드백을 받아 지속적으로 수정하여 보다 나은 포트폴리오로 발전시키는 과정을 가지고 있습니다.',
       },
     ],
     retrospective:
-      '기획부터 배포까지 혼자 진행하면서 TypeScript, Tailwind CSS 등 새로운 기술을 학습할 수 있는 좋은 경험이었습니다. 자세한 회고는 아래 링크에서 보실 수 있습니다.',
+      '기획부터 배포까지 혼자 진행하면서 TypeScript, Tailwind CSS 등 새로운 기술을 학습할 수 있는 좋은 경험이었습니다. 다른 개발자들의 피드백을 바탕으로 지속적으로 개선하고 있습니다. 자세한 회고와 트러블슈팅은 아래 링크에서 보실 수 있습니다.',
   },
   {
     title: 'SPA Router',
@@ -64,8 +101,7 @@ const projects: Projects[] = [
     imgUrl: Project_Router,
     url: {
       githubUrl: 'https://github.com/hyoungqu23/SPA_Router',
-      notionUrl: 'https://www.notion.so/SPA-Routing-524ab86f0720449a88b81bfdc353c10c',
-      retroUrl: 'https://hyoungmin.notion.site/SPA-Routing-524ab86f0720449a88b81bfdc353c10c',
+      retroUrl: 'https://minilog.vercel.app/blog/react-routing-without-library',
     },
     descriptions: [
       {
@@ -75,7 +111,7 @@ const projects: Projects[] = [
       },
     ],
     retrospective:
-      '손쉽게 사용했던 라이브러리를 직접 구현하면서 무심코 사용하고 있던 라우팅 기능을 더 깊게 이해할 수 있는 좋은 경험이었습니다. 추가적으로 다른 Hooks나 기능에 대해 비슷한 프로젝트를 진행하고자 합니다. 자세한 회고는 아래 링크에서 보실 수 있습니다.',
+      '손쉽게 사용했던 라이브러리를 직접 구현하면서 라우팅 기능에 대해 더 깊게 이해할 수 있는 좋은 경험이었습니다. 추가적으로 다른 Hooks나 기능에 대해 비슷한 프로젝트를 진행하고자 합니다. 자세한 회고와 트러블슈팅은 아래 링크에서 보실 수 있습니다.',
   },
   {
     title: '투자 관리 서비스 Admin',
@@ -86,14 +122,13 @@ const projects: Projects[] = [
     url: {
       githubUrl:
         'https://github.com/wanted-pre-onboarding-fe-6th-team2/pre-onboarding-assignment-week-4-1-team-2',
-      notionUrl: 'https://www.notion.so/admin-eeca6c576a0a4a68a155d3b7639c2856',
-      retroUrl: 'https://hyoungmin.notion.site/admin-eeca6c576a0a4a68a155d3b7639c2856',
+      retroUrl: 'https://minilog.vercel.app/blog/preonboarding-assignment-fint',
     },
     descriptions: [
       {
         tag: 'feature',
         content:
-          '로그인 시 사용자의 편의를 높이기 위해 로그인 만료 메시지 및 로그인 폼의 validation 기준 미충족시 경고 메시지 등을 구현했습니다.',
+          '로그인 시 사용자의 편의를 높이기 위해 로그인 만료 메시지 및 로그인 폼의 validation 기준 미충족시 경고 메시지 등을 추가적으로 구현했습니다.',
       },
       {
         tag: 'UI/UX',
@@ -102,7 +137,7 @@ const projects: Projects[] = [
       },
     ],
     retrospective:
-      '로그인 validation, 로그인 상태에 따른 페이지 라우팅 등의 로그인 기능을 구현하면서 인증/인가에 대해 이해할 수 있었고, 어떻게 하면 UX를 향상시킬 수 있을 지에 대해 고민하면서 기본적인 상세 페이지 UI를 구현했습니다. 자세한 회고는 아래 링크에서 보실 수 있습니다.',
+      '로그인 validation, 로그인 상태에 따른 페이지 라우팅 등의 로그인 기능을 구현하면서 인증/인가에 대해 이해할 수 있었고, 어떻게 하면 UX를 향상시킬 수 있을 지에 대해 고민하면서 기본적인 상세 페이지 UI를 구현했습니다. 자세한 회고와 트러블슈팅은 아래 링크에서 보실 수 있습니다.',
   },
   {
     title: '댓글 기능 프로젝트',
@@ -113,23 +148,22 @@ const projects: Projects[] = [
     url: {
       githubUrl:
         'https://github.com/wanted-pre-onboarding-fe-6th-team2/pre-onboarding-assignment-week-3-2-team-2',
-      notionUrl: 'https://www.notion.so/e93ad69d33d5476fba1a6f491c286c8a',
-      retroUrl: 'https://hyoungmin.notion.site/e93ad69d33d5476fba1a6f491c286c8a',
+      retroUrl: 'https://minilog.vercel.app/blog/preonboarding-assignment-sparkpet',
     },
     descriptions: [
       {
         tag: 'refactoring',
         content:
-          'Redux-Toolkit의 반복되는 ExtraReducers 코드를 제거하고자 extraReducerUtils를 고민하며 직접 구현해봤습니다.',
+          'Refactoring 과정에서 페어 프로그래밍을 하면서 Redux-Toolkit의 반복되는 ExtraReducers 코드를 제거하고자 extraReducerUtils를 직접 구현했습니다.',
       },
       {
         tag: 'refactoring',
         content:
-          'Redux 코드의 유지보수성과 가독성을 높이기 위해 Ducks Pattern을 도입하여 코드를 작성했습니다.',
+          'Redux 코드의 유지보수성과 가독성을 높이기 위해 디렉토리 구조를 고민하다가, Ducks Pattern을 도입하여 코드를 작성했습니다.',
       },
     ],
     retrospective:
-      '단순 강의나 공식 문서를 보면서 배우는 것보다 어느 정도 개념을 알게 되면 직접 부딫혀 보는 것의 중요성을 느낀 프로젝트입니다. 페어 프로그래밍 방식으로 진행하면서 Ducks Pattern 도입 여부와 Refactoring에 대해 고민하면서 Redux를 조금 더 이해할 수 있는 좋은 경험이었습니다. 자세한 회고는 아래 링크에서 보실 수 있습니다.',
+      '단순 강의나 공식 문서를 보면서 배우는 것보다 어느 정도 개념을 알게 되면 직접 코드를 작성해 보는 것의 중요성을 느낀 프로젝트입니다. 페어 프로그래밍 방식으로 진행하면서 Ducks Pattern 도입 여부와 Refactoring에 대해 고민하면서 Redux를 조금 더 이해할 수 있는 좋은 경험이었습니다. 자세한 회고와 트러블슈팅은 아래 링크에서 보실 수 있습니다.',
   },
   {
     title: '온라인 스토어 리뉴얼 프로젝트',
@@ -141,23 +175,22 @@ const projects: Projects[] = [
       pageUrl: 'https://fruitte-renewal.netlify.app/',
       githubUrl:
         'https://github.com/wanted-pre-onboarding-fe-6th-team2/pre-onboarding-assignment-week-1-2-team-2',
-      notionUrl: 'https://www.notion.so/8712c6543e664ee3931a4e07018e523f',
-      retroUrl: 'https://hyoungmin.notion.site/8712c6543e664ee3931a4e07018e523f',
+      retroUrl: 'https://minilog.vercel.app/blog/preonboarding-assignment-fruitte',
     },
     descriptions: [
       {
         tag: 'UI/UX',
         content:
-          '상품이 많아질수록 목록 렌더링 시간이 길어지므로, 페이지네이션 로직을 직접 구현하여 사용자의 대기 시간을 줄이고자 했습니다.',
+          '상품이 많아질수록 목록 렌더링 시간이 길어지므로, 사용자의 대기 시간을 줄이고자 페이지네이션 로직을 API 호출 없이 프론트단에서 직접 구현했습니다.',
       },
       {
         tag: 'UI/UX',
         content:
-          '상품의 제목과 설명이 줄바꿈이 되어 UI를 해치는 등의 기존 웹 사이트의 문제와 이미지 크기를 통일해 UI를 개선하고자 했습니다.',
+          '이미지의 통일성이 없는 점, 상품의 제목과 설명이 줄바꿈이 되어 UI를 해치는 점 등의 기존 웹 사이트의 문제점을 개선하고자 UI를 수정하여 구현했습니다.',
       },
     ],
     retrospective:
-      '특정 로직을 처음부터 뜯어보며 구현하는 것이 조금 더 깊게 이해하는데 도움을 준다는 것을 느끼고, UI/UX에 대해서 고민하게 만든 프로젝트입니다. 자세한 회고는 아래 링크에서 보실 수 있습니다.',
+      '페이지네이션 로직의 구조를 하나하나 확인하며 구현하면서 해당 로직을 조금 더 이해할 수 있게 되었고, 사소한 수정 사항이라도 UI/UX에 큰 영향을 미칠 수 있다는 것을 알게된 프로젝트입니다. 자세한 회고와 트러블슈팅은 아래 링크에서 보실 수 있습니다.',
   },
   {
     title: 'CFD',
@@ -168,7 +201,7 @@ const projects: Projects[] = [
     url: {
       githubUrl: 'https://github.com/hyoungqu23/CFD',
       notionUrl: 'https://www.notion.so/Community-For-Developers-f28f68db503d493bb46c755521fcc949',
-      retroUrl: 'https://hyoungmin.notion.site/CFD-ebc2c0899eb9405f88c20cdbf0f359d8',
+      retroUrl: 'https://minilog.vercel.app/blog/cfd-retrospective',
     },
     descriptions: [
       {
@@ -183,7 +216,7 @@ const projects: Projects[] = [
       },
     ],
     retrospective:
-      '라이브러리를 적용하는데 어려움을 겪었고, 협업 과정에서의 이슈로 시간적으로 여유가 부족한 와중에 끝까지 이슈를 해결하며 배포를 진행한 프로젝트입니다. 자세한 회고는 아래 링크에서 보실 수 있습니다.',
+      '에디터 라이브러리를 적용하는데 어려움을 겪었고, 백엔드와의 협업 과정에서 발생한 소통 이슈로 시간적으로 여유가 부족한 와중에 끝까지 이슈를 해결하며 배포를 진행한 프로젝트입니다. 자세한 회고와 트러블슈팅은 아래 링크에서 보실 수 있습니다.',
   },
   {
     title: 'Collector’s',
@@ -196,7 +229,7 @@ const projects: Projects[] = [
       githubUrl: 'https://github.com/Elice-Team-20/Collectors',
       notionUrl: 'https://www.notion.so/20-1335219f96a64e678f66152f45666389',
       figmaUrl: 'https://www.figma.com/file/MmTalHhD88uGEyQJGJKyoe/Pet-Shop?node-id=637%3A49',
-      retroUrl: 'https://hyoungmin.notion.site/Collector-s-2f607daa70124398a1c3146c76b9a466',
+      retroUrl: 'https://minilog.vercel.app/blog/collectors-retrospective',
     },
     descriptions: [
       {
@@ -216,7 +249,7 @@ const projects: Projects[] = [
       },
     ],
     retrospective:
-      '비동기적으로 협업하는 방식을 체득하고, 기본적인 웹 스토어의 기능을 구현하면서 재미있는 시간을 보낸 프로젝트입니다. 추가적으로 JavaScript로 진행하면서 React와 Next.js의 필요성을 느꼈습니다. 자세한 회고는 아래 링크에서 보실 수 있습니다.',
+      '팀에서 비동기적으로 협업하는 방식을 체득하고, 기본적인 웹 스토어의 기능을 구현하면서 재미있는 시간을 보낸 프로젝트입니다. 추가적으로 JavaScript로 진행하면서 React와 Next.js의 필요성을 느꼈습니다. 자세한 회고와 트러블슈팅은 아래 링크에서 보실 수 있습니다.',
   },
 ];
 
